81f6dfa81226dc980d623f8ae7d1a55a5d28d9ef
ALTERA-LLC <samscott640@gmail.com>
2020-09-06 09:44:38 +0800

    new commit

--- client.py
+++ client.py
@@ -3,8 +3,8 @@ import socket
 class Main:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.ip = 'altera-server.ddns.net'
-        self.sock.connect((self.ip, 8888))
+        self.ip = '58.160.92.128'
+        self.sock.connect((self.ip, 2288))
         self.whileloop()
 
 
--- server.py
+++ server.py
@@ -4,7 +4,7 @@ class Main:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.ip = 'localhost'
-        self.sock.bind((self.ip, 8888))
+        self.sock.bind((self.ip, 2288))
         self.sock.listen(1)
         print('hello')
         self.whileloop()
