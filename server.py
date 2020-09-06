91556551af2ef1e9ca7d50baa1933c1be0355c6f
ALTERA-LLC <samscott640@gmail.com>
2020-09-06 09:59:01 +0800

    new commit

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import socket
 class Main:
     def __init__(self):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.ip = 'localhost'
+        self.ip = '	192.168.0.69'
         self.sock.bind((self.ip, 2288))
         self.sock.listen(1)
         print('hello')
